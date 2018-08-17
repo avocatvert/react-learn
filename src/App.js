ae99a54c01b1f0446ba00cd7e58e716abce753b1
smbatchou <stephabio@yahoo.ca>
2018-08-17 18:53:31 -0400

    how to use list

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ class App extends Component {
 
   state = {
     persons: [
-      {name:'romeo', age:32},
-      {name:'pauline', age:27},
-      {name: 'viny', age:60}
+      {id: '1ac',name:'romeo', age:32},
+      {id: '1ab', name:'pauline', age:27},
+      {id: '1ad', name: 'viny', age:60}
     ],
     otherStates : 'will work on it',
     showPersons : true
   };
 
-  changeNameHandler = (event) => {
+  changeNameHandler = (event,persId) => {
+    const persIndex = this.state.persons.findIndex( (p) => {return p.id === persId;  });
+    //const person_match = { ...this.state.persons[persIndex] };  //copy one object person filtered by persIndex
+    // add target envent value to that person name;  person_match.name = event.target.value
+
+    const persons_ = [ ...this.state.persons ];
+    persons_[persIndex].name = event.target.value;
+
+    //upate state
     this.setState( 
     {
-      persons: [
-        {name: event.target.value, age: 32},
-        {name:'pauline' ,age: 27 },
-        {name: 'viny',age: 60}
-      ]
+      persons: persons_
     })
   };
 
@@ -33,6 +37,14 @@ class App extends Component {
       }
     )
   }
+
+  deletePersonHandler = (persIndex) => {
+    //const persons_ = this.state.persons; // a reference (not a copy)to state.persons (array)
+    const persons_ = [...this.state.persons] //or this.state.persons.slice() //will make a copy of the array
+    persons_.splice( persIndex,1);
+    this.setState({persons : persons_});
+  }
+
 
   render() {
     const buttonStyle = {
@@ -48,8 +60,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
       <div>
-        { this.state.persons.map(p => { 
-            return <Person name = {p.name} age = {p.age} />
+        { this.state.persons.map( (pers,index) => { 
+            return <Person 
+                    click={() => this.deletePersonHandler(index)} 
+                    changedName = {(event) => this.changeNameHandler (event,pers.id) }
+                    name = {pers.name} 
+                    age = {pers.age} 
+                    key = {pers.id}
+                    />
             } 
           )
         }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,9 +4,9 @@ import './Person.css';
 const person = (props) => { 
     return (
     <div className= "Person">
-        <p> My name is {props.name}, i am {props.age} years old </p> 
+        <p onClick={props.click}> My name is {props.name}, i am {props.age} years old </p> 
         <p> {props.children} </p> 
-        <input type='text' onChange={props.namechanged} value = {props.name}/>
+        <input type='text' onChange={props.changedName} value = {props.name}/>
     </div>
     );
 };
