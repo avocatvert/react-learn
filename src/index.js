773733133798f3cc26501d4d70bba8b9e17ccc32
avocatvert <stephabio@yahoo.ca>
2018-08-22 03:18:46 -0400

    minor fix index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App title="Relevant persons" />, document.getElementById('root'));
->>>>>>> ec1d48a88a26fa0dee21b2a6e8257ea41115037e
 registerServiceWorker();
